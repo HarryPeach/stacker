3868bc1172ad06bbe3b8a2b9c07a131cefef69e2
Harry Peach <harrypeach1999@gmail.com>
2020-02-04 19:58:11 +0000

    Speed now changes throughout game

--- main.cpp
+++ main.cpp
@@ -9,13 +9,31 @@
 #define WINDOW_WIDTH 30
 #define WINDOW_HEIGHT 20
 #define MAX_ROWS 15
-#define INIT_BLOCK_WIDTH 8
+#define INIT_BLOCK_WIDTH 14
 
 WINDOW *mainwindow;
 
 std::vector<Row> rows(MAX_ROWS, Row(WINDOW_WIDTH, 0));
 int currentRow = 0;
-int delay = 50;
+int delay = 45;
+
+int get_speed(int currentRow){
+	if (currentRow < 3){
+		return delay;
+	}else if (currentRow < 5){
+		return 40;
+	}else if (currentRow < 7){
+		return 35;
+	}else if (currentRow < 9){
+		return 30;
+	}else if (currentRow < 11){
+		return 25;
+	}else if (currentRow < 13){
+		return 20;
+	}else {
+		return 16;
+	}
+}
 
 int main()
 {
@@ -27,7 +45,6 @@ int main()
 
 	cbreak();
 	noecho();
-	timeout(delay);
 	clear();
 
 	draw_centered_string(WINDOW_WIDTH / 2, 2, "Stacker!");
@@ -40,6 +57,7 @@ int main()
 
 	rows.at(currentRow).setBlockWidth(INIT_BLOCK_WIDTH);
 	while(true){
+		timeout(delay);
 		char c = getch();
 		if (c == 'q'){
 			endwin();
@@ -54,7 +72,6 @@ int main()
 					break;
 			}else if(currentRow == 0){
 				rows.at(currentRow + 1).setBlockWidth(rows.at(currentRow).getBlockWidth());
-				currentRow++;
 			}else{
 				std::pair<int, int> infoPair = rows.at(currentRow).test_overlay(rows.at(currentRow - 1));
 				if(infoPair.first == -1){
@@ -66,13 +83,16 @@ int main()
 				rows.at(currentRow).setBlockWidth(infoPair.second);
 				rows.at(currentRow).moveBlock(infoPair.first);
 				rows.at(currentRow + 1).setBlockWidth(rows.at(currentRow).getBlockWidth());
-				currentRow++;
 			}
-			
+			delay = get_speed(currentRow);
+			currentRow++;
 		}
 
 		rows.at(currentRow).step();
 		draw_rows(1, WINDOW_HEIGHT, rows);
+		std::stringstream fmt;
+		fmt << "Delay: " << delay;
+		mvaddstr(1, 1, fmt.str().c_str());
 	}
 
 	timeout(-1);
